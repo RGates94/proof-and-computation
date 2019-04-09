0ee65135e8c974ce699be31db1cca2e99d5c7e42
RGates94 <rgates275@gmail.com>
2019-04-09 17:35:39 +0100

    Add compute method to ProgramState, to compute register machine on given input

--- src/main.rs
+++ src/main.rs
@@ -42,17 +42,25 @@ impl ProgramState {
 			}
         };
     }
+    fn compute(&mut self, machine: &RegisterMachine) {
+        while let Some(instruction) = machine.instructions.get(self.current_instruction) {
+            self.do_instruction(instruction);
+        };
+        self.halted = true;
+    }
 }
 
 fn main() {
     println!("Hello Proof and Computation, we will produce some proofs and computations!");
-    let basic_inc = RegisterInstruction::DEC(1, 7,4);
+    let register_machine = RegisterMachine {
+        instructions: vec![RegisterInstruction::INC(2, 1),RegisterInstruction::DEC(2, 2, 3),RegisterInstruction::INC(0, 1)]
+    };
     let mut basic_program_state = ProgramState {
         current_instruction: 0,
-        registers: vec![2, 3],
+        registers: vec![5, 3],
         halted: false,
     };
     println!("{:?}", basic_program_state);
-    basic_program_state.do_instruction(&basic_inc);
+    basic_program_state.compute(&register_machine);
     println!("{:?}", basic_program_state);
 }
