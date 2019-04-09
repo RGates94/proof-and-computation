fbc48a2b115061b6fb42657b6028f1ee2fa363c7
RGates94 <rgates275@gmail.com>
2019-04-10 00:09:33 +0100

    Create do_instruction method on WhileState, handle zero assignment case

--- src/while_program.rs
+++ src/while_program.rs
@@ -20,3 +20,36 @@ pub enum AssignType {
     Variable(String),
     VariableIncremented(String),
 }
+
+impl WhileState {
+    pub fn do_instruction(&mut self, instruction: &WhileInstruction) {
+        match instruction {
+            WhileInstruction::Assign(var, assign) => match assign {
+                AssignType::Zero => {
+                    self.variable_states.insert(var.to_string(), 0);
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn assign_zero() {
+        let instruction = WhileInstruction::Assign(String::from("x"), AssignType::Zero);
+        let second_instruction =
+            WhileInstruction::Assign(String::from("refrigerator"), AssignType::Zero);
+        let mut state = WhileState {
+            variable_states: HashMap::new(),
+        };
+        state.do_instruction(&instruction);
+        state.do_instruction(&second_instruction);
+        assert_eq!(state.variable_states.get("x"), Some(&0));
+        assert_eq!(state.variable_states.get("refrigerator"), Some(&0));
+    }
+}
